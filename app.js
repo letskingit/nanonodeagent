c3acd1f522c5d15b5c6572d002206b3dbe7f92fe
Aman Singh <besoeasy@users.noreply.github.com>
2020-08-17 13:22:26 +0100

    rate limiting

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const axios = require('axios').default;
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000,
+	max: 60,
+});
+
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 
-const allowedActions = ['account_history', 'account_info', 'accounts_frontiers', 'accounts_balances', 'accounts_pending', 'block', 'blocks', 'block_count', 'blocks_info', 'delegators_count', 'pending', 'process', 'representatives_online', 'validate_account_number'];
+const allowedActions = ['account_history', 'account_info', 'accounts_frontiers', 'accounts_balances', 'accounts_pending', 'block', 'blocks', 'block_count', 'blocks_info', 'bootstrap_status', 'delegators_count', 'pending', 'process', 'representatives_online', 'validate_account_number'];
 
 app.all('/', async (req, res) => {
 	console.log(req.body.action);
